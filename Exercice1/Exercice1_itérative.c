872659a91f7ca496f9f79e836f82f3c4d147a12f
VictorGrecoEns <victor.greco@ens-rennes.fr>
2024-01-26 09:20:47 +0100

    supression des fichiers binaires

---
+++ Exercice1/Exercice1_itérative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int* tableau_initial_pascal(){
+    int* tableau = malloc( sizeof(int)*3 );
+    tableau[0] = 0;
+    tableau[1] = 1;
+    tableau[2] = 0;
+    return tableau;
+}
+
+int* tableau_nouvelle_ligne_pascal(int* ligne, int n){
+    int* nouvelle_ligne = malloc(sizeof(int) * (n+1));
+    nouvelle_ligne[0] = 0;
+    nouvelle_ligne[n] = 0;
+    for (int i = 1; i < n; i++){
+        nouvelle_ligne[i] = ligne[i] + ligne[i-1];
+    }
+    return nouvelle_ligne;
+}
+
+void affiche_pascal(int n){
+    int* ligne_actuelle;
+    ligne_actuelle = tableau_initial_pascal();
+    for (int ligne = 0; ligne < n; ligne ++){
+        for (int colonne = 0; colonne < ligne; colonne++){
+            printf("%i ", ligne_actuelle[colonne + 1]);
+        }
+        printf("\n");
+        ligne_actuelle = tableau_nouvelle_ligne_pascal(ligne_actuelle, ligne + 3);
+    }
+}
+
+int main(){
+    int* tableau = tableau_initial_pascal();
+    free(tableau);
+}
